706f1f5cf18eefd8df713df1357f7b36febd8480
marco.wettstein <marco.wettstein@adcubum.com>
2025-12-01 12:52:28 +0100

    added second method (part two)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Select password method
-method = "0x0" # 0x0 = default, 0x434C49434B = newer
+method = "0x434C49434B" # 0x0 = default, 0x434C49434B = newer
 
 
 # Read input file
@@ -18,14 +18,29 @@ for line in lines:
     direction = line[0]
     value = int(line[1:])
     
+    # Store previous result
+    prev_result = result
+    
     # Update result based on direction
     if direction == 'L':
         result = (result - value) % 100
     else:  # 'R'
         result = (result + value) % 100
     
-    # Check if result is 0 and increment counter
-    if result == 0:
-        counter += 1
+    # Check if result is 0 and increment counter based on method
+    if method == "0x0":
+        if result == 0:
+            counter += 1
+    elif method == "0x434C49434B":
+        # Count if we land on 0
+        if result == 0:
+            counter += 1
+        # Count if we pass through 0
+        if direction == 'L':
+            if prev_result < value % 100:
+                counter += 1
+        else:  # 'R'
+            if prev_result + value >= 100:
+                counter += 1
 
 print(counter)
